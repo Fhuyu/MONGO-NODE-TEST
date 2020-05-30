8ed43d789cc276e71657b11093a3b06aed37db84
Fuyuh <aurore.chayrouse@gmail.com>
2020-05-30 10:44:48 +0200

    async way

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Character = require('./models/Character')
 const url = 'mongodb://127.0.0.1:27017/mydb'
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }) // useFind... is to make findOneAndUpdate work without warning
 
 const db = mongoose.connection
 db.once('open', _ => {
@@ -13,11 +13,55 @@ db.on('error', err => {
   console.error('connection error:', err)
 })
 
-const ryu = new Character ({
+// const ryu = new Character ({
+//     name: 'Ryu',
+//     ultimate: 'Shinku Hadoken'
+//   })
+//   ryu.save(function (error, document) {
+//     if (error) console.error(error)
+//     console.log(document)
+//   })
+
+async function runCode() {
+await Character.deleteMany({}) // Delete all content in collection
+
+  const ryu = new Character({
+    BattleID: 12,
     name: 'Ryu',
     ultimate: 'Shinku Hadoken'
   })
-  ryu.save(function (error, document) {
-    if (error) console.error(error)
-    console.log(document)
+  const ken = new Character({
+    BattleID: 13,
+    name: 'Ken',
+    ultimate: 'Guren Enjinkyaku'
   })
+
+  await ken.save()
+  await ryu.save()
+
+  const chars = await Character.find()
+  console.log(chars)
+  // VERSION 1 - FIND ITEM BEFORE CHANGING VALUE, THEN SAVE - to use
+  const ryu2 = await Character.findOne({ name: 'Ryu' })
+  ryu2.specials = [
+    'Hadoken',
+    'Shoryuken',
+    'Tatsumaki Senpukyaku'
+  ]
+  console.log(ryu2)
+  await ryu2.save()
+  // VERSION 2 - await findOneAndUpdate(filter, update)
+  await Character.findOneAndUpdate(
+    { name: 'Ken' },
+    {
+      specials: [
+        'Ken1',
+        'Shoryuken',
+        'Tatsumaki Senpukyaku'
+      ]
+    }).then(doc => console.log(doc)) // working but not showing in console log
+    
+}
+
+runCode()
+  .catch(error => { console.error(error) })
--- models/Character.js
+++ models/Character.js
@@ -2,8 +2,17 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const characterSchema = new Schema({
+  BattleID: { type: Number, unique: true },
   name: String,
+  specials: Array,
   ultimate: String
 })
 
 module.exports = mongoose.model('Character', characterSchema)
+
+/* String
+Number
+Boolean
+Array
+Date
+ObjectId */
